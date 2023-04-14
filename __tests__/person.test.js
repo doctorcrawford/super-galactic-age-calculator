5eab1aa928ba4ee95b4ac635f5704ab6fdd2d8e7
Kyle Crawford <kcraw217@gmail.com>
2023-04-14 09:22:27 -0700

    good fail testing to calculate persons age in mercury years

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,10 +1,19 @@
 import Person from './../src/js/person.js';
 
 describe('Person', () => {
-
+  let billy;
+  
+  beforeEach(() => {
+    billy = new Person("Billy", 31);
+  })
+  
   test('should correctly create a Person object with their name and age', () => {
-    const billy = new Person("Billy", 31);
     expect(billy.name).toEqual("Billy");
     expect(billy.age).toEqual(31);
+  });
+
+  test('should correctly calculate the persons age in Mercury years', () => {
+    billy.getSolarYears();
+    expect(billy.ageMercury).toEqual(129.17);
   });
 });
--- src/js/person.js
+++ src/js/person.js
@@ -3,4 +3,8 @@ export default class Person {
     this.name = name;
     this.age = age;
   } 
+
+  getSolarYears() {
+    
+  }
 }

216fec86578e1b7d9737021e134f1782268b64d9
Kyle Crawford <kcraw217@gmail.com>
2023-04-14 09:44:07 -0700

    pass test to calculate person age in mercury, venus, mars and jupiter years

--- src/js/person.js
+++ src/js/person.js
@@ -6,6 +6,8 @@ export default class Person {
 
   getSolarYears() {
     this.ageMercury = parseFloat((this.age / .24).toFixed(2));
-
+    this.ageVenus = parseFloat((this.age / .62).toFixed(2));
+    this.ageMars = parseFloat((this.age / 1.88).toFixed(2));
+    this.ageJupiter = parseFloat((this.age / 11.86).toFixed(2));
   }
 }

17e2a66d5fa61a0e9fe4861303f58dcaefc59faf
Kyle Crawford <kcraw217@gmail.com>
2023-04-14 16:44:03 -0700

    refactor planet calculations to method

--- src/js/person.js
+++ src/js/person.js
@@ -7,14 +7,18 @@ export default class Person {
     this.dob = new Date(dob);
   }
 
+  twoDigits(inputNum, yearDiv) {
+    return parseFloat((inputNum / yearDiv).toFixed(2));
+  }
+
   getAges() {
     const output = {};
     const today = new Date();
     output.ageEarth = today.getFullYear() - this.dob.getFullYear();
-    output.ageMercury = parseFloat((output.ageEarth / .24).toFixed(2));
-    output.ageVenus = parseFloat((output.ageEarth / .62).toFixed(2));
-    output.ageMars = parseFloat((output.ageEarth / 1.88).toFixed(2));
-    output.ageJupiter = parseFloat((output.ageEarth / 11.86).toFixed(2));
+    output.ageMercury = this.twoDigits(output.ageEarth, .24);
+    output.ageVenus = this.twoDigits(output.ageEarth, .62);
+    output.ageMars = this.twoDigits(output.ageEarth, 1.88);
+    output.ageJupiter = this.twoDigits(output.ageEarth, 11.86);
     return output;
   }
 
@@ -27,10 +31,10 @@ export default class Person {
     } else if (ageDiff < 0) {
       output.earthDiff = -(ageDiff);
     }
-    output.mercuryDiff = parseFloat(((output.earthDiff) / .24).toFixed(2));
-    output.venusDiff = parseFloat(((output.earthDiff) / .62).toFixed(2));
-    output.marsDiff = parseFloat(((output.earthDiff) / 1.88).toFixed(2));
-    output.jupiterDiff = parseFloat(((output.earthDiff) / 11.86).toFixed(2));
+    output.mercuryDiff = this.twoDigits(output.earthDiff, .24);
+    output.venusDiff = this.twoDigits(output.earthDiff, .62);
+    output.marsDiff = this.twoDigits(output.earthDiff, 1.88);
+    output.jupiterDiff = this.twoDigits(output.earthDiff, 11.86);
     return output;
   }
 }

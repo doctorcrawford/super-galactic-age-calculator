52885aed94f42db97effe394438d1db4e04a6647
Kyle Crawford <kcraw217@gmail.com>
2023-04-14 10:51:41 -0700

    WIP refactoring business logic to output object

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -2,11 +2,13 @@ import Person from './../src/js/person.js';
 
 describe('Person', () => {
   let billy;
+  let solarYears;
   
   beforeEach(() => {
     billy = new Person("Billy", 31);
-    billy.getSolarYears();
-  })
+    let solarYears = {};
+    solarYears = billy.getSolarYears();
+  });
   
   test('should correctly create a Person object with their name and age', () => {
     expect(billy.name).toEqual("Billy");
@@ -14,13 +16,14 @@ describe('Person', () => {
   });
 
   test('should correctly calculate the persons age in Mercury years', () => {
-    expect(billy.ageMercury).toEqual(129.17);
+    console.log("the return" + "solarYears");
+    expect(solarYears.ageMercury).toEqual(129.17);
   });
 
   test('should correctly calculate the persons age in years for Mercury, venus, mars and jupiter', () => {
-    expect(billy.ageMercury).toEqual(129.17);
-    expect(billy.ageVenus).toEqual(50);
-    expect(billy.ageMars).toEqual(16.49);
-    expect(billy.ageJupiter).toEqual(2.61);
+    expect(solarYears.ageMercury).toEqual(129.17);
+    expect(solarYears.ageVenus).toEqual(50);
+    expect(solarYears.ageMars).toEqual(16.49);
+    expect(solarYears.ageJupiter).toEqual(2.61);
   });
 });
--- src/js/person.js
+++ src/js/person.js
@@ -1,13 +1,18 @@
 export default class Person {
+  name = "";
+  age = undefined;
+
   constructor(name, age) {
     this.name = name;
     this.age = age;
-  } 
+  }
 
   getSolarYears() {
-    this.ageMercury = parseFloat((this.age / .24).toFixed(2));
-    this.ageVenus = parseFloat((this.age / .62).toFixed(2));
-    this.ageMars = parseFloat((this.age / 1.88).toFixed(2));
-    this.ageJupiter = parseFloat((this.age / 11.86).toFixed(2));
+    let output = {};
+    solarYears.ageMercury = parseFloat((this.age / .24).toFixed(2));
+    solarYears.ageVenus = parseFloat((this.age / .62).toFixed(2));
+    solarYears.ageMars = parseFloat((this.age / 1.88).toFixed(2));
+    solarYears.ageJupiter = parseFloat((this.age / 11.86).toFixed(2));
+    return output;
   }
 }
